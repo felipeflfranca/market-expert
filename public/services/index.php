b799366651a473593785e861f03915bd1f67677d
Felipe <felipe.flfranca@gmail.com>
2021-12-06 23:17:52 -0300

    cors

--- public/services/index.php
+++ public/services/index.php
@@ -1,4 +1,8 @@
 <?php
+header("Access-Control-Allow-Origin: http://localhost:3000");
+header("Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Accept, Authorization, X-Requested-With, X-Auth-Token, Origin, Application");
+
 header('Content-Type: application/json');
 
 require_once '../../vendor/autoload.php';
