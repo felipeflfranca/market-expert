95e5c453a35b7d38d65479d215e6c3573c802570
Felipe <felipe@multiway.com.br>
2021-12-10 17:52:21 -0300

    Cart

--- resources/react-code/src/views/dashboard/ProductsSales.js
+++ resources/react-code/src/views/dashboard/ProductsSales.js
@@ -4,7 +4,7 @@ import { useState } from 'react';
 import config from 'config';
 
 // material-ui
-import { CardContent, Grid } from '@mui/material';
+import { Box, Grid } from '@mui/material';
 import Paper from '@mui/material/Paper';
 import InputBase from '@mui/material/InputBase';
 import Divider from '@mui/material/Divider';
@@ -13,12 +13,12 @@ import ClearIcon from '@mui/icons-material/Clear';
 
 // project imports
 import MainCard from 'ui-component/cards/MainCard';
-import { gridSpacing } from 'store/constant';
 import ProductItem from '../../ui-component/products';
 
 // ==============================|| PRODUCT SALES ||============================== //
 
 const ProductsSales = () => {
+    const [search, addSearch] = useState('');
     const [products, setProducts] = useState([]);
 
     async function searchProduct(search) {
@@ -37,12 +37,14 @@ const ProductsSales = () => {
     }
 
     const clearSearch = () => {
-        console.log('teste');
         setProducts([]);
+        addSearch('');
     };
 
     const productSearch = (e) => {
         const value = e.target.value;
+
+        addSearch(value);
 
         if (value === '') {
             clearSearch();
@@ -52,45 +54,51 @@ const ProductsSales = () => {
     };
 
     return (
-        <MainCard content={false} sx={{ height: '80vh' }}>
-            <CardContent>
-                <Grid container spacing={gridSpacing}>
-                    <Grid item xs={12} sx={{ pt: '16px !important' }}>
-                        <Paper
-                            component="form"
-                            sx={{
-                                p: '2px 4px',
-                                display: 'flex',
-                                alignItems: 'center',
-                                width: '100%',
-                                border: '1px solid #c7c7c7'
-                            }}
-                        >
-                            <InputBase
-                                sx={{ ml: 1, flex: 1, width: '100%' }}
-                                placeholder="Buscar produto"
-                                inputProps={{ 'aria-label': 'Buscar Produto' }}
-                                onChange={productSearch}
-                                autoFocus
-                            />
-                            <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
-                            <IconButton color="primary" sx={{ p: '10px' }} aria-label="directions" type="button" onClick={clearSearch}>
-                                <ClearIcon />
-                            </IconButton>
-                        </Paper>
-                    </Grid>
-                    <Grid item xs={12}>
-                        {!Array.isArray(products)
-                            ? products
-                            : products.map((product) => (
-                                  <div key={`search-${product.code}`}>
-                                      <ProductItem key={product.code} product={product} addToBagVisible quantity={0} />
-                                      <Divider sx={{ my: 1.5 }} />
-                                  </div>
-                              ))}
-                    </Grid>
-                </Grid>
-            </CardContent>
+        <MainCard ontent={false} sx={{ height: 'calc(100vh - 131px)', position: 'relative' }}>
+            <Grid item xs={12} sx={{ pt: '16px !important' }}>
+                <Paper
+                    component="form"
+                    sx={{
+                        p: '2px 4px',
+                        display: 'flex',
+                        alignItems: 'center',
+                        width: '100%',
+                        border: '1px solid #c7c7c7'
+                    }}
+                >
+                    <InputBase
+                        sx={{ ml: 1, flex: 1, width: '100%' }}
+                        placeholder="Buscar produto"
+                        inputProps={{ 'aria-label': 'Buscar Produto' }}
+                        value={search}
+                        onChange={productSearch}
+                        autoFocus
+                    />
+                    <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+                    <IconButton color="primary" sx={{ p: '10px' }} aria-label="directions" type="button" onClick={clearSearch}>
+                        <ClearIcon />
+                    </IconButton>
+                </Paper>
+            </Grid>
+            <Grid item xs={12} style={{ marginTop: '10px' }}>
+                <Box
+                    style={{
+                        maxHeight: 'calc(80vh - 60px)',
+                        overflow: 'auto',
+                        marginTop: '10px 10px 10px 10px',
+                        padding: '10px 10px 10px 10px'
+                    }}
+                >
+                    {!Array.isArray(products)
+                        ? products
+                        : products.map((product) => (
+                              <div key={`search-${product.code}`}>
+                                  <ProductItem key={product.code} product={product} addToBagVisible quantity={0} />
+                                  <Divider sx={{ my: 1.5 }} />
+                              </div>
+                          ))}
+                </Box>
+            </Grid>
         </MainCard>
     );
 };
